f948c3c5858fe9a5d94050c66519edb3ed980fcb
Alexander <marrruska0@gmail.com>
2020-05-25 22:30:54 +0300

    Stage 2/4: Words and sentences

--- Main.java
+++ Main.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        sb.append(scanner.nextLine());
-        if (sb.length() <= 100)
-            System.out.println("EASY");
-        else
+        String[] array = scanner.nextLine().split("[.?!]");
+        int number = 0;
+        for (int i = 0; i < array.length; i++) {
+            number += array[i].split(" ").length;
+        }
+        if (number/array.length > 10)
             System.out.println("HARD");
+        else
+            System.out.println("EASY");
     }
 }
